17b84a27098673f1d4a44b83cbcf37501c6c5740
Snomysnow <187812257+Snomysnow@users.noreply.github.com>
2025-05-12 00:11:29 -0400

    adjusted compose router

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
-var composeRouter = require ('./route/compose')
+var composeRouter = require('./routes/compose');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
-app.use('/compose', composeRouter)
+app.use('/compose', composeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/compose.js
+++ routes/compose.js
@@ -1,9 +1,23 @@
 var express = require('express');
 const fs = require("fs");
 var router = express.Router();
+
 const postDBFileName = "./model/postDB.json";
 
 router.get('/', function(req, res, next) {
-  res.render('./auth/compose');
+  res.render('./main/compose', {msg: ""});
 });
 
+router.post('/submit', function(req,res,next){
+    if(!req.body.title || req.body.body){
+        return renderCompose(req, res, "Fields cannot be empty.");
+    }
+});
+
+
+
+function renderCompose(req, res, msg){
+    res.render('./main/compose', {msg: msg});
+}
+
+module.exports = router;

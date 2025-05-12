252bd300841e2efa25599a5bd9e5b81426949f69
Snomysnow <187812257+Snomysnow@users.noreply.github.com>
2025-05-12 00:39:35 -0400

    compose workings

--- routes/compose.js
+++ routes/compose.js
@@ -9,15 +9,39 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/submit', function(req,res,next){
-    if(!req.body.title || req.body.body){
+    console.log(req.body)
+    if(!req.body.title || !req.body.body){
         return renderCompose(req, res, "Fields cannot be empty.");
     }
+    let postDB = readPostDB();
+    for (let posts of postDB.posts){
+        if (posts.title == req.body.title){
+            return renderCompose(req, res, "Title is already used");
+        }
+    }
+
+    postDB.posts.push(req.body);
+    writePostDB(postDB.posts);
+    renderHome(req, res);
 });
-
-
 
 function renderCompose(req, res, msg){
     res.render('./main/compose', {msg: msg});
+}
+
+function readPostDB() {
+    let data = fs.readFileSync(postDBFileName, "utf-8");
+    console.log(data);
+    return JSON.parse(data);
+}
+
+function writePostDB(post){
+    let data = JSON.stringify(post, null, 2);
+    fs.writeFileSync(postDBFileName, data, "utf-8");
+}
+
+function renderHome(req, res) {
+    res.redirect("/feed");
 }
 
 module.exports = router;
